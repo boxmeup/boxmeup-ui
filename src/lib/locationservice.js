45e9d2597a214f8e3e6ca0e4fdf8a5a5ae3ffd39
Chris Saylor <cjsaylor@gmail.com>
2017-09-20 07:21:46 -0400

    Only display locations attached to containers in container view.

--- src/lib/locationservice.js
+++ src/lib/locationservice.js
@@ -4,10 +4,10 @@ export default class LocationService {
     }
 
     async allLocations() {
-        const response = await this.fetch('/api/location?sort_field=id');
+        const response = await this.fetch('/api/location?sort_field=id&is_attached_to_container=T');
         const pageRequests = Array(response.meta.pages).fill(1).map((i, j) => j + 1)
         pageRequests.shift();
-        const rest = await Promise.all((pageRequests || []).map(page => this.fetch(`/api/location?sort_field=id&page=${page}`)));
+        const rest = await Promise.all((pageRequests || []).map(page => this.fetch(`/api/location?sort_field=id&is_attached_to_container=T&page=${page}`)));
         const flattened = rest.map(entry => entry.locations).reduce((acc, current) => acc.concat(current), []);
         return (response.locations || []).concat(flattened);
     }
