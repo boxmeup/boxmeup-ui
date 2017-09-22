ac7b7154926aeb42b50dba33ebb9abf7b8578d2e
Chris Saylor <cjsaylor@gmail.com>
2017-09-22 07:15:17 -0400

    Remove options dropdown.
    Added tooltip for modified column to read exact date.

--- src/components/containers/ContainerRow.jsx
+++ src/components/containers/ContainerRow.jsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import {
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle,
     Input,
     Label,
-    UncontrolledDropdown
+    UncontrolledTooltip
 } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import moment from 'moment';
@@ -24,16 +21,9 @@ export default class ContainerRow extends Component {
                 <td><Label check><Input type="checkbox" value={this.props.container.id} onChange={this.onSelected} /></Label></td>
                 <td className="text-center"><Link to={'/containers/view/' + this.props.container.id}>{this.props.container.name}</Link></td>
                 <td className="text-center hidden-sm-down">{this.props.container.container_item_count}</td>
-                <td className="text-center hidden-sm-down">{moment(this.props.container.modified).fromNow()}</td>
-                <td className="text-right">
-                    <UncontrolledDropdown tether>
-                        <DropdownToggle caret>
-                            More
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem>Delete Container</DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
+                <td className="text-center hidden-sm-down">
+                    <span id={"tt-mod-" + this.props.container.id}>{moment(this.props.container.modified).fromNow()}</span>
+                    <UncontrolledTooltip placement="top" target={"tt-mod-" + this.props.container.id}>{this.props.container.modified}</UncontrolledTooltip>
                 </td>
             </tr>
         );
